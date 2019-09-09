262637bc54fa86b895f960eb8fa5eea1118dc239
m7Aei <mateuspm2015@hotmail.com>
2019-09-09 17:11:50 -0300

    Fazendo a validaçao dos campos na criaçao do user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const validation = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await validation.isValid(req.body))) {
+      return res.status(400).json({ error: 'Erro na validação' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
