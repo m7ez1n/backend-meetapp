b6cc7c3e72cffd67bea6df5f40e136a8826ed123
m7Aei <mateuspm2015@hotmail.com>
2019-09-09 17:30:31 -0300

    Fazendo o metodo update do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -31,6 +31,52 @@ class UserController {
       email,
     });
   }
+
+  async update(req, res) {
+    const validation = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await validation.isValid(req.body))) {
+      return res.status(400).json({ error: 'Erro na validação' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: email });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Usuário já existe' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Sua senha antiga está incorreta' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
 }
 
 export default new UserController();
