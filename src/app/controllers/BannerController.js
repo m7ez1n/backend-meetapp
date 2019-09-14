ab29beb1c138150bd04f971b68d73bd67679abe5
m7Aei <mateuspm2015@hotmail.com>
2019-09-14 01:38:50 -0300

    Criando o controller do banner do meetup

---
+++ src/app/controllers/BannerController.js
@@ -0,0 +1,16 @@
+import Banner from '../models/Banner';
+
+class BannerController {
+  async store(req, res) {
+    const { originalname: name, filename: path } = req.banner;
+
+    const banner = await Banner.create({
+      name,
+      path,
+    });
+
+    return res.json(banner);
+  }
+}
+
+export default new BannerController();
