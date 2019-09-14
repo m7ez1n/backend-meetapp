39b03b1ec45d397b7604debe82b0643e496615d4
m7Aei <mateuspm2015@hotmail.com>
2019-09-14 01:41:40 -0300

    Carregando os models no index da database

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,12 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Meetup from '../app/models/Meetup';
+import Banner from '../app/models/Banner';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Meetup, Banner];
 
 class Database {
   constructor() {
