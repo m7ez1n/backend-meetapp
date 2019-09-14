cb77f2be2076ec744a91e31c93833da5a1ac778e
m7Aei <mateuspm2015@hotmail.com>
2019-09-14 01:42:27 -0300

    Criando a rota do meetup e banner

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middleware/auth';
+import BannerController from './app/controllers/BannerController';
+import MeetupController from './app/controllers/MeetupController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,5 +22,9 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/users/avatar', upload.single('file'), FileController.store);
+
+routes.post('/meetup', MeetupController.store);
+
+routes.post('/meetup/banner', upload.single('banner'), BannerController.store);
 
 export default routes;
