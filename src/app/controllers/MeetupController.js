34fb72e45fcc88776fd2fd1e262626fd78008317
m7Aei <mateuspm2015@hotmail.com>
2019-09-16 14:24:19 -0300

    Pegando o id do criador do meetup

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,10 +1,30 @@
+import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
-    const meetup = await Meetup.create(req.body);
+    const validation = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
 
-    res.json(meetup);
+    if (!(await validation.isValid(req.body))) {
+      return res.status(400).json({ error: 'Erro na validação' });
+    }
+
+    const { title, description, location, date } = req.body;
+
+    const meetup = await Meetup.create({
+      creator_id: req.userId,
+      title,
+      description,
+      location,
+      date,
+    });
+
+    return res.json(meetup);
   }
 }
 
