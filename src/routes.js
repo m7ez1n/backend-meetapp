d0f13a65f93390efccdaf53d49b458affc1c380a
m7Aei <mateuspm2015@hotmail.com>
2019-09-16 16:14:02 -0300

    Criando rotas para update no meetup

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,10 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
-
-import authMiddleware from './app/middleware/auth';
 import BannerController from './app/controllers/BannerController';
 import MeetupController from './app/controllers/MeetupController';
+
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,6 +24,7 @@ routes.put('/users', UserController.update);
 routes.post('/users/avatar', upload.single('file'), FileController.store);
 
 routes.post('/meetup', MeetupController.store);
+routes.put('/meetup/:id', MeetupController.update);
 
 routes.post('/meetup/banner', upload.single('banner'), BannerController.store);
 
