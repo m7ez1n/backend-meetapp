6bacd8f7df71db924aba6162c3491891c200533d
m7Aei <mateuspm2015@hotmail.com>
2019-09-16 21:00:09 -0300

    Adicionando o campo 'as' na associaçao do meetup

--- src/app/models/Meetup.js
+++ src/app/models/Meetup.js
@@ -18,7 +18,7 @@ class Meetup extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.Banner, { foreignKey: 'banner_id' });
+    this.belongsTo(models.Banner, { foreignKey: 'banner_id', as: 'banner' });
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.User, { foreignKey: 'creator_id', as: 'creator' });
   }
